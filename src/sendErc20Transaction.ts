0913f60b480783dac31b131dc488756fde070b87
MatthiasWanner <matthias.wanner@gmail.com>
2022-04-12 12:34:16 +0200

    :bulb: (doc-code) Correct english mistake in the code source doc

--- src/sendErc20Transaction.ts
+++ src/sendErc20Transaction.ts
@@ -8,7 +8,7 @@ import erc20Abi from './constants/erc20_abi.constants';
  * @param args.amount have to be applied with decimals multiplicator (e.g: to send 10 erc20 tokens with 18 decimals, args.amount = 10 * 10**18)
  * @param args.contractAddress of the sent ERC20 token
  * @param args.windowEthereum is the metamask provider
- * @param args.receiverAddress is the address to send the ERC20 tokens
+ * @param args.receiverAddress is the address to which we send the ERC20 tokens
  * @return string txHash
  */
 export const sendErc20Transaction = async (
